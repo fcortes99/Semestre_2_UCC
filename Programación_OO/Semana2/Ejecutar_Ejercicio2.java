1a09fe7dd11a48c83a6d3da738ef3eab588e8ac6
fcortes99 <felipe.cortes@campusucc.edu.co>
2026-09-09 02:15:59 +0000

    Clase_2 _Clases

---
+++ Programación_OO/Semana2/Ejecutar_Ejercicio2.java
@@ -0,0 +1,10 @@
+public class Ejecutar_Ejercicio2 {
+
+public static void main(String[] args) {
+    //creacion del objeto Objejercicio2 (estudianteclase)
+
+    Ejercicio2 obEjercicioa = new Ejercicio2(1998, 1107589632, "Felipe", "Cortes", 28, 1.87);
+
+    System.out.println(obEjercicioa); // mostrar la informacion que tiene el objeto objejecicioa
+}    
+}
--- Programación_OO/Semana2/Ejercicio2.java
+++ Programación_OO/Semana2/Ejercicio2.java
@@ -1,5 +1,30 @@
-package Programación_OO.Semana2;
-
 public class Ejercicio2 {
     
+    //Atributos
+    private int id;
+    private int cedula;
+    private String nombre;
+    private String apellidos;
+    private int edad;
+    private double estatura;
+
+    // Constructor: el metodo constructor de la clase se reconoce porque tiene el mismo nombre de la clase.
+    // permite darle vida al objeto
+    
+    public Ejercicio2(int id, int cedula, String nombre, String apellidos, int edad, double estatura ){
+
+        this.id = id;
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+        this.estatura = estatura;
+
+    }
+
+    public String toString(){
+
+        return  "Ejercicio2 {id: "+ id + " cedula: " + cedula + " nombre: " + nombre + " apellidos: " + apellidos + " edad: " + edad + " estatura: " + estatura;
+    }
+
 }
